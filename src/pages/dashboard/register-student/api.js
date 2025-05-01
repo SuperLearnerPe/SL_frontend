14f1463af9781ece2d92b0008d5dcd5d665eb586
IvyedSG <deyvioscarzanches@gmail.com>
2025-04-30 23:08:07 -0500

    ok

--- src/pages/dashboard/register-student/api.js
+++ src/pages/dashboard/register-student/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://backend-as-sp-426148382897.us-central1.run.app"
+const BASE_URL = "https://backend-superlearner-1083661745884.us-central1.run.app"
 
 export const getStudents = async (page = 1, pageSize = 10) => {
   try {
